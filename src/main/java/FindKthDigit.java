30bfcffe07859b86bbbc8f089d71cde6700359b5
neblancsi <neblancsi95@gmail.com>
2024-12-13 08:13:29 +0100

    main method added

--- src/main/java/FindKthDigit.java
+++ src/main/java/FindKthDigit.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class FindKthDigit {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int q = sc.nextInt();
+		sc.nextLine();
+
+		StringBuilder actualOutput = new StringBuilder();
+		for (int i = 1; i <= q; i++) {
+			long k = Long.parseLong(sc.nextLine());
+			actualOutput.append(FindKthDigit.findKthDigit(k)).append("\n");
+		}
+
+		System.out.println(actualOutput);
+	}
+
 	public static char findKthDigit(long k) {
 		return findKthDigitRecursive(k, 1, 1);
 	}
